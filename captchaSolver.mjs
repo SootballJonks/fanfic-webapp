daf5fefa11f82dba4854bd58b21168495ad106c5
Jonks <anastasiablackhold@gmail.com>
2023-03-18 18:35:47 -0700

    ffnet scraper progress

---
+++ captchaSolver.mjs
@@ -0,0 +1,68 @@
+// captchaSolver.js
+import fetch from 'node-fetch';
+
+class CaptchaSolver {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
+  
+  async solveRecaptcha(siteKey, url) {
+    const apiUrl = `https://2captcha.com/in.php?key=${this.apiKey}&method=turnstile&sitekey=${siteKey}&pageurl=${url}&json=1`;
+
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+
+
+    if (result.status !== 1) {
+      return { errorId: 1, errorMessage: 'Error sending captcha to 2captcha' };
+    }
+
+    const requestId = result.request;
+
+    for (let i = 0; i < 24; i++) {
+      await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds
+
+      const checkUrl = `https://2captcha.com/res.php?key=${this.apiKey}&action=get&id=${requestId}&json=1`;
+      const checkResponse = await fetch(checkUrl);
+      const checkResult = await checkResponse.json();
+
+      if (checkResult.status === 1) {
+        return { errorId: 0, solution: checkResult.request };
+      }
+    }
+
+    return { errorId: 1, errorMessage: 'Timeout solving captcha' };
+  }
+
+  // async solveCloudflareTurnstile(url) {
+  //   const apiUrl = `https://2captcha.com/in.php?key=${this.apiKey}&method=cfstream&url=${encodeURIComponent(url)}&json=1`;
+
+  //   const response = await fetch(apiUrl);
+  //   const result = await response.json();
+  //   console.log("result: ", result)
+
+  //   if (result.status !== 1) {
+  //     return { errorId: 1, errorMessage: 'Error sending Cloudflare Turnstile request to 2captcha' };
+  //   }
+
+  //   const requestId = result.request;
+
+  //   for (let i = 0; i < 24; i++) {
+  //     await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds
+
+  //     const checkUrl = `https://2captcha.com/res.php?key=${this.apiKey}&action=get&id=${requestId}&json=1`;
+  //     const checkResponse = await fetch(checkUrl);
+  //     const checkResult = await checkResponse.json();
+
+  //     if (checkResult.status === 1) {
+  //       return { errorId: 0, solution: checkResult.request };
+  //     }
+  //   }
+
+  //   return { errorId: 1, errorMessage: 'Timeout solving Cloudflare Turnstile' };
+  // }
+}
+
+
+
+export default CaptchaSolver;
--- ffScraper.mjs
+++ ffScraper.mjs
@@ -1,22 +1,24 @@
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
+import CaptchaSolver from './captchaSolver.mjs';
+// import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
 // import UserPreferencesPlugin from 'puppeteer-extra-plugin-user-preferences';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const RECAPTCHA_API_KEY = process.env.RECAPTCHA_API_KEY; // Replace with your API key
+const captchaSolver = new CaptchaSolver(RECAPTCHA_API_KEY);
 
 puppeteer.use(StealthPlugin());
-puppeteer.use(
-  RecaptchaPlugin({
-    provider: {
-      id: '2captcha',
-      token: RECAPTCHA_API_KEY,
-    },
-    visualFeedback: true,
-  }),
-);
+// puppeteer.use(
+//   RecaptchaPlugin({
+//     provider: {
+//       id: '2captcha',
+//       token: RECAPTCHA_API_KEY,
+//     },
+//     visualFeedback: true,
+//   }),
+// );
 // puppeteer.use(UserPreferencesPlugin({
 //   userPrefs: {
 //     credential_enable_service: false,
@@ -26,9 +28,41 @@ puppeteer.use(
 //   },
 // }));
 
+async function solveCaptcha(page) {
+  try {
+    // Wait for the reCAPTCHA element to be available before getting the siteKey
+    await page.waitForSelector('.g-recaptcha', { timeout: 10000 });
+
+    const siteKey = await page.evaluate(() => {
+      return document.querySelector('.g-recaptcha')?.getAttribute('data-sitekey');
+    });
+
+    if (siteKey) {
+      const url = page.url();
+      const captchaResponse = await captchaSolver.solveRecaptcha(siteKey, url);
+
+      if (captchaResponse.errorId === 0) {
+        await page.evaluate(
+          (response) => {
+            document.getElementById('g-recaptcha-response').innerHTML = response;
+          },
+          captchaResponse.solution
+        );
+        await page.click('#challenge-form button');
+        return true;
+      }
+    }
+  } catch (error) {
+    console.log('reCAPTCHA element not found or timed out:', error.message);
+  }
+
+  return false;
+}
+
+
 async function ffScrape() {
   // Set up Puppeteer browser
-  const browser = await puppeteer.launch({ headless: false }); // Change to headless: true when done debugging
+  const browser = await puppeteer.launch({ headless: false }); 
   const page = await browser.newPage();
 
   // // CLOUDFLARE CAPTCHA BYPASS
@@ -41,11 +75,27 @@ async function ffScrape() {
   await page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
 
   // Introduce a delay to allow the Cloudflare challenge page to load
-  await page.waitForTimeout(10000); // 5000 == 5 seconds delay
+  await page.waitForTimeout(5000); // delay to load captcha
+  const captchaSolved = await solveCaptcha(page);
+  await page.waitForTimeout(5000); // delay to submit captcha
 
-  await page.solveRecaptchas();
-  // await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 0 });
+  if (captchaSolved) {
+    console.log('Captcha solved.');
+    await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 0 });
+  } else {
+    console.log('Captcha not detected or not solved.');
+  }
 
+
+  // //Check for Cloudflare challenge page
+  // const cloudflareChallengeSelector = '#challenge-form';
+  // const hasCloudflareChallenge = await page.$(cloudflareChallengeSelector);
+  // if (hasCloudflareChallenge) {
+  //   console.log('Cloudflare challenge detected, solving reCAPTCHA...');
+  //   await page.solveRecaptchas();
+  //   console.log('Cloudflare challenge solved.');
+  //   // await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 0 });
+  // }
 
   // Collect text from the 'profile_top' element
   const profileTopSelector = '#profile_top';
